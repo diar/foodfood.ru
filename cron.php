ba75a6d132bf61c4d2d6e8a95fd620c74f672a45
valmon <valmon@xakep.ru>
2010-06-18 17:35:47 +0400

    fix rest_view

--- cron.php
+++ cron.php
@@ -1,8 +1,8 @@
 <?php
 setlocale (LC_CTYPE, "ru_RU.UTF-8");
 header('Content-Type: text/html; charset=utf-8');
-chdir('/var/www/foodfood');
-$_SERVER['DOCUMENT_ROOT']='/var/www/foodfood';
+chdir('/var/www/foodfood.ru');
+$_SERVER['DOCUMENT_ROOT']='/var/www/foodfood.ru';
 $_SERVER['REQUEST_URI'] = '/kazan/';
 include_once ('engine/core/autoload.php');
 // Код, запускаемый каждый день в 12:00
