6caa934938c59a3123473c674174596cc00cf161
Z-MODe <Z-MODe@yandex.ru>
2010-06-24 14:44:04 +0400

    blog

--- blog/config/config.local.php
+++ blog/config/config.local.php
@@ -1,18 +1,4 @@
 <?php
-/*-------------------------------------------------------
-*
-*   LiveStreet Engine Social Networking
-*   Copyright © 2008 Mzhelskiy Maxim
-*
-*--------------------------------------------------------
-*
-*   Official site: www.livestreet.ru
-*   Contact e-mail: rus.engine@gmail.com
-*
-*   GNU General Public License, version 2:
-*   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-*
----------------------------------------------------------
 */
 /**
  * Настройки для локального сервера.
