f7fb33fb768a9c537112381b5232f98a8df008e9
valmon <valmon@xakep.ru>
2010-06-26 13:14:25 +0400

    fix discount widget

--- pages/widget.php
+++ pages/widget.php
@@ -20,7 +20,7 @@ class widget_Page extends View {
     public static function discountAction ($id) {
         Debug::disable();
         Cache::disable();
-        $discounts=MD_Discount::getDiscountBlock(array('count'=>5));
+        $discounts=MD_Discount::getDiscountBlock(array('count'=>7));
 
         // Добавляем переменные xslt
         self::$page['content']['discounts'] = $discounts;
